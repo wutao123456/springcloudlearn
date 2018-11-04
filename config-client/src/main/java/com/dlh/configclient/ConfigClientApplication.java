0fec8eea523e93171c83ab4c029ee92bd364e2a0
wutao123456 <wutao56789@gmail.com>
2018-11-04 22:56:10 +0800

    config-client配置消息总线

--- config-client/src/main/java/com/dlh/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/dlh/configclient/ConfigClientApplication.java
@@ -3,11 +3,17 @@ package com.dlh.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
+@EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
