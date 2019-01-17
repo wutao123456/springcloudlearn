edf1b5851289074fd2adc3a84a12564c5bb8cafb
wutao123456 <wutao56789@gmail.com>
2019-01-17 23:54:53 +0800

    add springcloud-docker

---
+++ eureka-client-docker/src/main/java/com/wutao/eureka/EurekaClientDockerApplication.java
@@ -0,0 +1,14 @@
+package com.wutao.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientDockerApplication.class, args);
+    }
+
+}
+
